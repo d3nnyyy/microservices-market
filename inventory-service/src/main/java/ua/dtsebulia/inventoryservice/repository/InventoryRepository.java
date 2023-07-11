ffbfa151d1d1d68cef608916c967c7a0e573fe9f
d3nnyyy <d3nnn41k@gmail.com>
2023-07-11 18:12:51 +0300

    set inter process communication between order and inventory

--- inventory-service/src/main/java/ua/dtsebulia/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/ua/dtsebulia/inventoryservice/repository/InventoryRepository.java
@@ -3,10 +3,10 @@ package ua.dtsebulia.inventoryservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.dtsebulia.inventoryservice.model.Inventory;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    Optional<Inventory> findBySkuCode(String skuCode);
-
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
