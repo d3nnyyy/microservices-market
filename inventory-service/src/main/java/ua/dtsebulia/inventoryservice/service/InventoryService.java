a51dfa40e4ae3bb3545a86b16012a4367651a6c9
d3nnyyy <d3nnn41k@gmail.com>
2023-07-13 13:52:25 +0300

    added circuit breaker to avoid delays in communications between components

--- inventory-service/src/main/java/ua/dtsebulia/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/ua/dtsebulia/inventoryservice/service/InventoryService.java
@@ -1,6 +1,8 @@
 package ua.dtsebulia.inventoryservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.dtsebulia.inventoryservice.dto.InventoryResponse;
@@ -12,12 +14,17 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
--- order-service/src/main/java/ua/dtsebulia/orderservice/controller/OrderController.java
+++ order-service/src/main/java/ua/dtsebulia/orderservice/controller/OrderController.java
@@ -1,10 +1,15 @@
 package ua.dtsebulia.orderservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ua.dtsebulia.orderservice.dto.OrderRequest;
 import ua.dtsebulia.orderservice.service.OrderService;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,9 +20,19 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order placed successfully!";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(
+                () -> orderService.placeOrder(orderRequest)
+        );
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(
+                () -> "Unfortunately, at the moment you cannot place order. Please, try later!"
+        );
     }
 
 }
